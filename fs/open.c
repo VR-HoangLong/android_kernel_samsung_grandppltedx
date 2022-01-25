fd1f78a5c3c6018f068c2bb5f7e6f41e274b25bd
Almond <iwanttobebetter7@gmail.com>
2022-01-25 10:12:26 +0700

    fs/open: add missing function dentry_open
    Signed-off-by: Almond

--- fs/open.c
+++ fs/open.c
@@ -842,6 +842,37 @@ int vfs_open(const struct path *path, struct file *filp,
 }
 EXPORT_SYMBOL(vfs_open);
 
+struct file *dentry_open(const struct path *path, int flags,
+			 const struct cred *cred)
+{
+	int error;
+	struct file *f;
+
+	validate_creds(cred);
+
+	/* We must always pass in a valid mount pointer. */
+	BUG_ON(!path->mnt);
+
+	f = get_empty_filp();
+	if (!IS_ERR(f)) {
+		f->f_flags = flags;
+		error = vfs_open(path, f, cred);
+		if (!error) {
+			/* from now on we need fput() to dispose of f */
+			error = open_check_o_direct(f);
+			if (error) {
+				fput(f);
+				f = ERR_PTR(error);
+			}
+		} else { 
+			put_filp(f);
+			f = ERR_PTR(error);
+		}
+	}
+	return f;
+}
+EXPORT_SYMBOL(dentry_open);
+
 static inline int build_open_flags(int flags, umode_t mode, struct open_flags *op)
 {
 	int lookup_flags = 0;
